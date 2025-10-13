f2087c5898644aa99203931254881b79aae94efc
AkiraGoto2 <akiragoto20005@gmail.com>
2025-10-13 10:15:28 +0500

    a4

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@ Route::get('/reports',
 [ReportController::class, 'index'])
 ->name('reports.index');
 
-Route::get('/reports/create', function () {
-    return view('report.create');
-})->name('reports.create');
+Route::get('/reports/create', function () { return view('reports.create'); })->name('reports.create');
+
+Route::get('/reports/{report}',[ReportController::class,'show'])->name('reports.show');
+
+Route::get('/reports/{report}/edit',[ReportController::class,'edit'])->name('reports.edit');
+
+Route::put('/reports/{report}',[ReportController::class,'update'])->name('reports.update');
+
+Route::post('/reports', [ReportController::class, 'store'])->name('reports.store');
+
+Route::delete('/reports/{report}',[ReportController::class, 'destroy'])->name('reports.destroy');
+
+
+
